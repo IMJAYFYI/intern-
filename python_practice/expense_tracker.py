8f5102145c6d59983338d0534a710cbd5e4349dc
jay kumar <jaymali7976@gmail.com>
2026-05-13 09:09:45 +0530

    add expense tracker app

---
+++ python_practice/expense_tracker.py
@@ -0,0 +1,75 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from datetime import date
+from typing import Literal
+
+app = FastAPI()
+
+class Transaction(BaseModel):
+    type: Literal['income', 'expense']
+    amount: float = Field(gt=0)
+    category: str
+    date: date
+
+transactions = []
+
+@app.post('/transactions')
+def add_transaction(transaction: Transaction):
+    transaction.category = transaction.category.lower()
+    transactions.append(transaction)
+    return {"message": "Transaction added successfully!", "data": transaction}
+
+@app.get('/transactions')
+def get_transactions(category: str = None):
+    if category == None:
+        return transactions
+    
+    search_category = category.lower()
+    filtered_list = []
+    
+    for item in transactions:
+        if item.category == search_category:
+            filtered_list.append(item)
+            
+    return filtered_list
+
+@app.get('/balance')
+def get_balance():
+    total_income = 0
+    total_expense = 0
+    
+    for item in transactions:
+        if item.type == 'income':
+            total_income = total_income + item.amount
+        elif item.type == 'expense':
+            total_expense = total_expense + item.amount
+            
+    final_balance = total_income - total_expense
+    
+    return {
+        'income': total_income,
+        'expense': total_expense,
+        'balance': final_balance
+    }
+
+@app.get('/summary/{year}/{month}')
+def get_monthly_summary(year: int, month: int):
+    monthly_income = 0
+    monthly_expense = 0
+    
+    for item in transactions:
+        if item.date.year == year and item.date.month == month:
+            if item.type == 'income':
+                monthly_income = monthly_income + item.amount
+            elif item.type == 'expense':
+                monthly_expense = monthly_expense + item.amount
+                
+    monthly_balance = monthly_income - monthly_expense
+    
+    return {
+        "year": year,
+        "month": month,
+        "income": monthly_income,
+        "expense": monthly_expense,
+        "balance": monthly_balance
+    }
